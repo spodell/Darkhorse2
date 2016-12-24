62cfdca1ba297092ea7cf6265b7716b7e1c65941
spodell <spodell@ucsd.edu>
2016-12-23 20:26:31 -0800

    Update load_lineage_index_table.pl

--- bin/installation_scripts/load_lineage_index_table.pl
+++ bin/installation_scripts/load_lineage_index_table.pl
@@ -154,11 +154,13 @@ if (defined $overwrite || $found_table == 0)
 	my $total_num = $1;
 	print STDERR "total num = $total_num\n";
 	my @skipped_lines = ();
-	$max_lines_per_packet = 2000;
-	if (defined $update)
+	if (defined $overwrite || $found_table == 0)
 	{
-		$max_lines_per_packet = 1;
-	
+		$lines_per_packet = $max_lines_per_packet;
+	}
+	elsif (defined $update)
+	{
+		$lines_per_packet = 1;
 	}
 
 # compose database population command, send insert statement packets
